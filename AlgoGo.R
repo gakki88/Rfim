e249f1306f493ff80a25862c1159e625dfb81b0c
gakki88 <tangtang5yo@gmail.com>
2018-04-20 16:08:13 +0200

    3rd commit Algo

--- AlgoGo.R
+++ AlgoGo.R
@@ -1,35 +1,56 @@
-source("D:\\yuxin\\AlgoMultip\\FIM.R")
-
-it<-1000
-t<-exp(c(-50:50)/10)
-parnum<-4
-w<-rep(1/length(t),length(t))
-
-#MF<-funFIMsm("c+(de-c)/(1+exp(b*log(t)-b*log(a)))",c("c","de","b","a"),c(0,1,1,1),c(1,0),list(exp(c(-50:50)/10)),0,c(1))
-
-MFi<-list()
-for(l in 1:length(t)){
-  MFi[[l]] <-funFIMsm("c+(de-c)/(1+exp(b*log(t)-b*log(a)))",c("c","de","b","a"),c(0,1,1,1),c(1,0),list(t[l]),0,c(1))
-
-}
-
-for(k in 1:it){
-  mw<-list()
-  Mw<-matrix(rep(0),nrow=parnum+1,ncol=parnum+1)
- for(i in 1:length(t)){
-   mw[[i]]<-w[i]*MFi[[i]]
-   Mw<-Mw+mw[[i]]
- }
+AlgoMult<-function(model,paramName,paramValue,sigma,design_Init,lambda,dose,nbSubjects,iteration){
+  source("D:\\yuxin\\AlgoMultip\\FIM.R")
+  cat("********************Avant optimiser*******************\n")
+  funFIMsm(model,paramName,paramValue,sigma,design_Init,dose,nbSubjects)
+  w<-rep(1/length(design_Init[[1]]),length(design_Init[[1]]))
   
-  m<-solve(Mw)
-  critd<-det(Mw)^(1/parnum)     # D-criterion value
-  Dphi <- critd * solve(Mw)/parnum             # calculate derivatives of function phi_D
- d<-c()
- for(j in 1:length(t)){
-   d<-c(d,sum(diag(Dphi %*% MFi[[j]])))
- }
-
-  w<- w*d^0.99/sum(w*d^0.99)
+  
+  MFi<-list()
+  for(l in 1:length(design_Init[[1]])){
+    MFi[[l]] <-funFIMsm(model,paramName,paramValue,sigma,design_Init[[1]][l],dose,nbSubjects)
+    
+  }
+  
+  it<-0
+  for(k in 1:iteration){
+    mw<-list()
+    if((sigma[1]==0 && sigma[2]!=0) || (sigma[2]==0 && sigma[1]!=0)){
+      Mdim=length(paramName)+1
+    }
+    if(sigma[1]==0 && sigma[2]==0){
+      Mdim=length(paramName)
+    }
+    if(sigma[1]!=0 && sigma[2]!=0){
+      Mdim=length(paramName)+2
+    }
+    Mw<-matrix(rep(0),nrow=Mdim,ncol=Mdim)
+    for(i in 1:length(design_Init[[1]])){
+      mw[[i]]<-w[i]*MFi[[i]]
+      Mw<-Mw+mw[[i]]
+    }
+    #m<-solve(Mw)
+    critd<-det(Mw)^(1/Mdim)     # D-criterion value
+    Dphi <- critd * solve(Mw)/Mdim            # calculate derivatives of function phi_D
+    d<-c()
+    for(j in 1:length(design_Init[[1]])){
+      d<-c(d,sum(diag(Dphi %*% MFi[[j]])))
+    }
+    w1<-w
+    w<- w*d^lambda/sum(w*d^lambda)
+    if((w-w1)<1e-09){break}
+    it<-it+1
+  }
+  
+  v<-which(w>1/length(paramName)*0.7)
+  cat("\n\n*********************Apres optimiser****************\n")
+  funFIMsm(model,paramName,paramValue,sigma,list(design_Init[[1]][v]),dose,nbSubjects)
+  
+  cat("\n**************** Result of Algorithm ****************\n")
+  
+  #return(list(w,v,design_Init[[1]][v],it))
+  cat("******************* WEIGHT ******************\n",w,
+      "\n\n******************* IMPORTANT WEIGHT INDEX ******************\n", v,
+      "\n\n******************* IMPORTANT WEIGHT VALUE ******************\n",design_Init[[1]][v],
+      "\n\n******************* NUMBER OF INTERATIONS ******************\n",it,
+      "\n\n******************* D-CRITERION ******************\n",critd*length(v))
 }
-w
-plot(w,ylim=c(0,1))
--- TestAlgo.R
+++ TestAlgo.R
@@ -1,9 +1,13 @@
-source("D:\\insa\\inserm\\AlgoMultip\\AlgoMult.R")
+source("D:\\yuxin\\AlgoMultip\\AlgoGo.R")
 
-w<-AlgoMult("c+(de-c)/(1+exp(b*log(t)-b*log(a)))",c("c","de","b","a"),c(0,1,1,1),c(1,0),list(exp(c(-50:50)/10)),0.99,0,c(1),500)
+w<-AlgoMult("c+(de-c)/(1+exp(b*log(t)-b*log(a)))",c("c","de","b","a"),c(0,1,1,1),c(1,0),list(exp(c(-50:50)/10)),0.99,0,c(1),10000)
 w
-plot(w,ylim=c(0,1))
+plot(w[[1]],ylim=c(0,1))
 
-w1<-AlgoMult("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.6,0.07),list(c(0.5, 1, 2, 6, 9, 12, 24, 36, 48, 72, 96, 120)),0.99,c(100),c(1),100)
-w1
-plot(w1,ylim=c(0,1))
+res<-AlgoMult("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.6,0.07),list(c(0.5, 1, 2, 6, 9, 12, 24, 36, 48, 72, 96, 120)),0.99,c(100),c(1),3000)
+res
+
+plot(res[[1]],ylim=c(0,1))
+
+dd<-Algo
+  funFIMsm("Emax*t/(t+C50)+S0",c("Emax","C50","S0"),c(30,500,5),c(1,0),list(c(0,100 , 300 , 500 , 1000 , 2500 , 5000)),0,c(1))
