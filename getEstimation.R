76f4124f7e7a310351a100f5d14ae233642da514
gakki88 <tangtang5yo@gmail.com>
2018-07-05 17:14:02 +0200

    for 3rd simulation

--- getEstimation.R
+++ getEstimation.R
@@ -38,25 +38,64 @@ names(SEemp) <- vecName
 paramValue<-c(55,0.005,0.2,30,150)
 omega<-c(0.07,0.5,1,150)
 sigma_i<-sqrt(28)
-ree<-(gg[1,]-paramValue[1])/paramValue[1]*100
+ree<-c((gg[1,]-paramValue[1])/paramValue[1]*100)
+ree2<-ree
 for(i in 2:5){
-  ree<-cbind(ree,(gg[i,]-paramValue[i])/paramValue[i]*100)
+  ree<-c(ree,(gg[i,]-paramValue[i])/paramValue[i]*100)
+  ree2<-cbind(ree2,(gg[i,]-paramValue[i])/paramValue[i]*100)
 }
 for(i in 1:4){
-  ree<-cbind(ree,(gg[5+i,]-omega[i])/omega[i]*100)
+  ree<-c(ree,(gg[5+i,]-omega[i])/omega[i]*50)
+  ree2<-cbind(ree2,(gg[5+i,]-omega[i])/omega[i]*50)
 }
-ree<-cbind(ree,(gg[10,]-sigma_i)/sigma_i*100)
-
-names(ree)<-vecName
-#Boxplots ree
+ree<-c(ree,(gg[10,]-sigma_i)/sigma_i*100)
+ree2<-cbind(ree2,(gg[10,]-sigma_i)/sigma_i*100)
+ree1 <- data.frame(values=ree, parameters = rep(vecName, each = 500))
+#colnames(ree)<-vecName
+#Boxplots ree#######################
 #library(Rlab)
-boxplot(ree,names=c(expression(mu["VA"[0]]),expression(mu["k"]),expression(mu[beta]),expression(mu["E"["max"]]),expression(mu["ED"[50]]),expression(omega["VA"]^{2}),expression(omega["k"]^{2}),expression(omega[beta]^{2}),expression(omega["E"["max"]]^{2}),expression(sigma["i"])))
+boxplot(ree2,names=c(expression(mu["VA"[0]]),expression(mu["k"]),expression(mu[beta]),expression(mu["E"["max"]]),expression(mu["ED"[50]]),expression(omega["VA"]^{2}),expression(omega["k"]^{2}),expression(omega[beta]^{2}),expression(omega["E"["max"]]^{2}),expression(sigma["i"])))
 #bplot(ree,style="quantile",labels=c("µ_VA","µ_k","µ_be","µ_Emax","µ_ED50","w2_VA","w2_k","w2_be","w2_Emax","w2_ED50","sigma_i"))
-abline(h=0,col='red')
+
+
+f <- function(x) {
+  r <- quantile(x, probs = c(0.05, 0.25, 0.5, 0.75, 0.95))
+  names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
+  r
+}
+customerr <- function(x) {
+  r <- quantile(x, probs = c(0.05,  0.95))
+  names(r) <- c("ymin", "ymax")
+  r
+}
+
+library(ggplot2) #,fill=parameters
+
+ree1$parameters <- factor(ree1$parameters, levels = ree1$parameters)
+ggplot(ree1) +aes(x=parameters, y=values,color="") +
+  theme_bw(15)+
+  labs(y="REE")+
+  stat_summary(fun.data = f, geom='boxplot',position="dodge")+
+  geom_hline( yintercept=0)+
+  stat_summary(fun.data = customerr, geom="errorbar",position=position_dodge(0.9),size=1, width=0.6) +
+  scale_x_discrete( labels=c(expression(mu["VA"[0]]),expression(mu["k"]),expression(mu[beta]),expression(mu["E"["max"]]),expression(mu["ED"[50]]),expression(omega["VA"]),expression(omega["k"]),expression(omega[beta]),expression(omega["E"["max"]]),expression(sigma["i"])))
+
+#ggsave("D:\\yuxin\\Simulation2\\resultatSimulation\\ree11111.png")
+
+#ggplot(ree1)+aes(x=parameters, y=values,fill=parameters)+  geom_boxplot(fill='#A4A4A4', color="darkred")
+#ggsave("D:\\yuxin\\Simulation2\\resultatSimulation\\ree11.png")
+#abline(h=0,col='red')
+#ggsave("D:\\yuxin\\Simulation2\\resultatSimulation\\ree1.png")
 
 # par(mfrow=c(1,1))
-RB<-apply(ree,2,mean)
+ree2<-ree[1:500]
+for(i in 2:10)
+{
+  ree2<-cbind(ree2,ree[(500*(i-1)+1):(500*i)])
+}
+RB<-apply(ree2,2,mean)
 names(RB)<-vecName
+RB1<-RB[c(3,5,4,2,1,10,8,9,7,6)]
 
 RSE<-100 * SEemp / c(paramValue,omega,sigma_i)
 
@@ -65,7 +104,7 @@ fimVA<-funFIMpop("VA+(1-exp(-k*t))*(emax*dose/(ED50+dose)-be*VA)",c("VA","k","be
 RSEfim<-fimVA[[5]]
 
 RSE2<-rbind(RSE,RSEfim)
-barplot(RSE2,beside = T,legend=T,ylim=c(0,60),names=c(expression(mu["VA"[0]]),expression(mu["k"]),expression(mu[beta]),expression(mu["E"["max"]]),expression(mu["ED"[50]]),expression(omega["VA"]^{2}),expression(omega["k"]^{2}),expression(omega[beta]^{2}),expression(omega["E"["max"]]^{2}),expression(sigma["i"])))
+barplot(RSE2,beside = T,col=c("darkblue","red"),legend=T,ylim=c(0,60),names=c(expression(mu["VA"[0]]),expression(mu["k"]),expression(mu[beta]),expression(mu["E"["max"]]),expression(mu["ED"[50]]),expression(omega["VA"]^{2}),expression(omega["k"]^{2}),expression(omega[beta]^{2}),expression(omega["E"["max"]]^{2}),expression(sigma["i"])))
 
 par(mfrow=c(2,2))
 for(i in 1:10){
