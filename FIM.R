5146cb2f06bd1316af23af3dc0a6a2d113c96c81
gakki88 <tangtang5yo@gmail.com>
2018-07-31 13:18:06 +0200

    "dernier" version

--- FIM.R
+++ FIM.R
@@ -1,47 +1,43 @@
-funFIMsm <- function(equation,paramName,parameter,sigma,t_indiv,d,PropSubjects,nbTot){
+funFIMind <- function(equation,parameters,beta,sigma,t_indiv,d,PropSubjects,nbTot){
 #Name of the fixed effects parameters and variable sampling times
-paramF<-c(paramName,"t")
-
-#model equation
-form1 <- equation
-equatf <- parse(text = form1, n=-1)
-f<-function(paramF){eval(equatf[[1]])}
-
-form2 <- paste0("( sig.inter + sig.slope * fixed )^2")
-Vmodel<- parse(text = form2)
-
-PSI<-c(paramName,"sig.inter","sig.slope")
+ParametersInEquation<-c(parameters,"t")
+#All parameters to be considered
+PSI<-c(parameters,"sig.inter","sig.slope")
 lpsi <- length(PSI)
-lengthParameters<-length(paramName)
+lengthParameters<-length(parameters)
+
+#Prepare to compute values of model equation with fixed effect parameters
+equatf <- parse(text = equation, n=-1)
+f<-function(ParametersInEquation){eval(equatf[[1]])}
+
+#residual error model
+Vmodel<- parse(text = paste0("( sig.inter + sig.slope * fixed )^2"))
 
 dose<-d
 
 #Fixed effects parameters values
-beta <- parameter
 for(i in 1:lengthParameters){
-  assign(paramName[i],beta[i])
+  assign(parameters[i],beta[i])
 }
 
+#Parameters of standard deviation model of residual error (sig.inter+sig.slope*f)^2:
+sig.inter<-sigma[1]
+sig.slope<-sigma[2]
 
 M_f<-list()
 M_F <- matrix(rep(0),nrow=lpsi,ncol=lpsi)
 #gather all groups of protocol
 for(q in 1:length(t_indiv)){
   #sample times for an individual design
-  t<-c()
-  t<-c(t,t_indiv[[q]])
+  t<-t_indiv[[q]]
 
-
-  #Standard deviation of residual error (sig.inter+sig.slope*f)^2:
-  sig.inter<-sigma[1]
-  sig.slope<-sigma[2]
   
-  param <- c(beta,t)
-  fixed<-f(param)
+  parameterValues <- c(beta,t)
+  fixed<-f(parameterValues)                  #Fixed effect values
   if(length(t)==1){
-    var<-(sig.inter+sig.slope*fixed)^2
+    var<-(sig.inter+sig.slope*fixed)^2       #Variance of residual error for one (measurement/sampling time)/dose
   }else{
-    var<-diag((sig.inter+sig.slope*fixed)^2)
+    var<-diag((sig.inter+sig.slope*fixed)^2) #Variance of resudual error for all times/doses
   }
   
 
@@ -65,10 +61,9 @@ for(q in 1:length(t_indiv)){
   
   M_A <- t(mdf) %*% solve(var) %*% mdf 
   M_B <- matrix(rep(0),nrow=lpsi,ncol=lpsi)
+  
   for(i in 1:2){
-    for(j in 1:2){
-      M_B[lengthParameters+i,lengthParameters+j] <- 1/2 * sum(diag((m[[i]] %*% solve(var) %*% m[[j]] %*% solve(var))))
-    }
+    M_B[lengthParameters+i,c(lpsi-1,lpsi)] <- sapply(m, function(x) 1/2 * sum(diag(m[[i]] %*% solve(var) %*% x %*% solve(var) )))
   }
   M_f[[q]] <- (M_A+M_B)*PropSubjects[q]
   M_F <-M_F+M_f[[q]]
@@ -77,18 +72,13 @@ for(q in 1:length(t_indiv)){
 M_F<-M_F*nbTot
 
 #set names for vectors
-fname<-c()
-for(n in 1:lengthParameters){
-  fname<-c(fname,paste0("u_",paramName[n]))
-}
-
-fname<-c(fname,"sig.inter","sig.slope")
+fname <-c(sapply(1:lengthParameters, function(x) paste0("u_",parameters[x])),"sig.inter","sig.slope")
 rownames(M_F) <- fname
 colnames(M_F) <- fname
 
 if(0 %in% sigma){
-  M_F<-M_F[-c(lengthParameters+which(c(sigma)==0)),-c(lengthParameters+which(c(sigma)==0))]
-  PSI<- PSI[-c(lengthParameters+which(sigma==0))]
+  M_F <- M_F[-c(lengthParameters+which(c(sigma)==0)),-c(lengthParameters+which(c(sigma)==0))]
+  PSI <- PSI[-c(lengthParameters+which(sigma==0))]
 }
 
 if(length(t)==1){
@@ -98,27 +88,26 @@ if(length(t)==1){
   deterFim <- det(M_F)
   SE <- sqrt(diag(solve(M_F)))
   RSE <- 100 * SE / c(beta,sigma)[which(c(beta,sigma)!=0)]
-  CritereDopt <- deterFim^(1/lpsi)
+  CritereDopt <- deterFim^(1/length(c(beta,sigma)[which(c(beta,sigma)!=0)]))
 
   #write the output in the console
-  # cat("******************* FISHER INFORMATION MATRIX ******************\n")
-  # print(M_F)
-  # 
-  # cat("\n\n******************* DETERMINANT OF THE MATRIX ******************\n", deterFim,
-  #     "\n\n******************* STANDARD ERROR ******************\n",SE,
-  #     "\n\n******************* RELATIVE STANDARD ERROR ******************\n",RSE,
-  #     "\n\n******************* CRITERION ******************\n",CritereDopt,"\n")
+#   cat("******************* FISHER INFORMATION MATRIX ******************\n")
+#   print(M_F)
+#   
+#   cat("\n\n******************* DETERMINANT OF THE MATRIX ******************\n", deterFim,
+#       "\n\n******************* CRITERION ******************\n",CritereDopt,
+#       "\n\n******************* STANDARD ERROR ******************\n",SE,
+#       "\n\n******************* RELATIVE STANDARD ERROR ******************\n",RSE,"\n")
   
   #suppose just one group of design, plot the function and result points
-
-  t<-t_indiv[[1]]
-  x1<-c(beta,t)
-  fpoints<-f(x1)
-  plot(t,fpoints) #first plot the result points
-  t<-seq(min(t),max(t),1)
-  x<-c(beta,t)
-  fvalue<-f(x)
-  lines(t,fvalue) #then plot the line of function
+#   t<-t_indiv[[1]]
+#   x1<-c(beta,t)
+#   fpoints<-f(x1)
+#   plot(t,fpoints,xlab="Dose",ylab="Effect") #first plot the result points
+#   t<-seq(min(t)-0.5,max(t),1)
+#   x<-c(beta,t)
+#   fvalue<-f(x)
+#   lines(t,fvalue) #then plot the line of function
   
   return(list(M_F,deterFim,CritereDopt,SE,RSE))
 }
@@ -129,10 +118,11 @@ if(length(t)==1){
 
 
 #exercice1
-#funFIMsm("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.6,0.07),list(c(0.5, 1, 2, 6, 9, 12, 24, 36, 48, 72, 96, 120)),c(100),c(1),32)
+# funFIMind("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.6,0.07),list(c(0.5, 1, 2, 6, 9, 12, 24, 36, 48, 72, 96, 120)),c(100),c(1),32)
 
 #exercice2
-#funFIMsm("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.6,0.07),list(c(0.5, 2, 9, 24, 48, 96),c(1, 6, 12, 36, 72, 120)),c(100),c(0.5,0.5),32)
+# funFIMind("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.6,0.07),list(c(0.5, 2, 9, 24, 48, 96),c(1, 6, 12, 36, 72, 120)),c(100),c(0.5,0.5),32)
 
 #emample dose-response
-#funFIMsm("Emax*t/(t+C50)+S0",c("Emax","C50","S0"),c(30,500,5),c(1,0),list(c(0,100 , 300 , 500 , 1000 , 2500 , 5000)),0,c(1),1)
+# funFIMind("Emax*t/(t+D50)+E0",c("Emax","D50","E0"),c(30,500,5),c(1,0),list(c(0,100 , 300 , 500 , 1000 , 2500 , 5000)),0,c(1),1)
+ 
--- FIMmixte.R
+++ FIMmixte.R
@@ -1,4 +1,4 @@
-funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjects,nbTot){
+funFIMpop <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjects,nbTot){
   #List of names of the fixed effects parameters and sampling times
   ParametersInEquation<-c(parameters,"t")
 
@@ -9,6 +9,13 @@ funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjec
   #(Diagonal Matrix of) variance for inter-subject random effects:
   omega<-diag(o)
   
+  #Parameters of standard deviation model of residual error (sig.inter+sig.slope*f)^2:
+  sig.inter<-sigma[1]
+  sig.slope<-sigma[2]
+  
+  #get derivatives of sigma
+  Vmodel<- parse(text = paste0("( sig.inter + sig.slope * fixed )^2"))
+  dv<-deriv(Vmodel[[1]],PSI)
   
   #gather all groups of protocol
   M_f<-list()
@@ -33,23 +40,15 @@ funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjec
     parameterValues <- c(beta,t)
     fixed<-f(parameterValues)
     
-    #Standard deviation of residual error (sig.inter+sig.slope*f)^2:
-    sig.inter<-sigma[1]
-    sig.slope<-sigma[2]
-    
-    var<-diag((sig.inter+sig.slope*fixed)^2)
-    
-    
     #calculate variance Var
-    form2 <- paste0("( sig.inter + sig.slope * fixed )^2")
-    Vmodel<- parse(text = form2)
+    var<-diag((sig.inter+sig.slope*fixed)^2)
     
     #get derivatives for fixed parameters
     df<-deriv(equatf[[1]],PSI)
     mdf<-attributes(eval(df))$gradient
     #delete the last two columns (correspond to sig.inter and sig.slope) 
     mdfi <- mdf[,-c(lpsi-1,lpsi)]
-    #complete derivative for exponential random effect model   
+    #complete derivative for random effect model   
     #Random effect model (1) = additive  (2) = exponential 
     #------------------------------------------------------------------
     beta0 <- beta
@@ -60,9 +59,7 @@ funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjec
     Vi <- mdfie %*% omega %*% t(mdfie) + var
     #inverse of matrix Vi
     SVi <- solve(Vi)
-    
-    #get derivatives of sigma
-    dv<-deriv(Vmodel[[1]],PSI)
+    #compute derivatives of sigma
     mdv<-attributes(eval(dv))$gradient
     
     
@@ -117,7 +114,15 @@ funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjec
       deterFim <- det(M_F)
       SE <- sqrt(diag(solve(M_F)))
       RSE <- 100 * SE / c(beta,o,sigma)[which(c(beta,o,sigma)!=0)]
-      CritereDopt <- deterFim^(1/(lpsi+lengthParameters))
+      CritereDopt <- deterFim^(1/length(c(beta,o,sigma)[which(c(beta,o,sigma)!=0)]))
+      #write the output in console
+#       cat("******************* FISHER INFORMATION MATRIX ******************************************************\n")
+#       print(M_F)
+#       
+#       cat("\n\n******************* DETERMINANT OF THE MATRIX **************************************************\n", deterFim,
+#           "\n\n******************* CRITERION ******************************************************************\n",CritereDopt,
+#           "\n\n******************* STANDARD ERROR *************************************************************\n",SE,
+#           "\n\n******************* RELATIVE STANDARD ERROR ****************************************************\n",RSE,"\n\n")
       return(list(M_F,deterFim,CritereDopt,SE,RSE))
     },error=function(e){
       return(list(M_F,det(M_F)))
@@ -128,16 +133,6 @@ funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjec
 #     x<-c(beta,t)
 #     fvalue<-f(x)
 #     lines(t,fvalue) #then plot the line of function
-
-    #write the output in console
-    # cat("******************* FISHER INFORMATION MATRIX ******************\n")
-    # print(M_F)
-    # 
-    # cat("\n\n******************* DETERMINANT OF THE MATRIX ******************\n", deterFim,
-    #     "\n\n******************* STANDARD ERROR ******************\n",SE,
-    #     "\n\n******************* RELATIVE STANDARD ERROR ******************\n",RSE,
-    #     "\n\n******************* CRITERION ******************\n",CritereDopt)
-    
   }
   
   
@@ -145,10 +140,10 @@ funFIMem <- function(equation,parameters,beta,o,sigma,t_group,Trand,d,PropSubjec
   
 }
 #exercice1
-#funFIMem("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.7,0.02,0.06),c(0.6,0.07),list(c(0.5, 1, 2, 6, 9, 12, 24, 36, 48, 72, 96, 120)),c(2,2,2),c(100),c(1),32)
+# funFIMpop("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.7,0.02,0.06),c(0.6,0.07),list(c(0.5, 1, 2, 6, 9, 12, 24, 36, 48, 72, 96, 120)),c(2,2,2),c(100),c(1),32)
 
 #exercice2
-#funFIMem("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.7,0.02,0.06),c(0.6,0.07),list(c(0.5, 2, 9, 24, 48, 96),c(1, 6, 12, 36, 72, 120)),c(2,2,2),c(100,100),c(0.5,0.5),32)
+# funFIMpop("dose/V*ka/(ka-(Cl/V))*(exp(-(Cl/V)*t)-exp(-ka*t))",c("ka","V","Cl"),c(1.6,8,0.13),c(0.7,0.02,0.06),c(0.6,0.07),list(c(0.5, 2, 9, 24, 48, 96),c(1, 6, 12, 36, 72, 120)),c(2,2,2),c(100,100),c(0.5,0.5),32)
 
 #exemple dose-reponse
-#funFIMem("Emax*t/(t+C50)+S0",c("Emax","C50","S0"),c(30,500,5),c(0.09,0.09,0.09),c(1,0),list(c(0,100 , 300 , 500 , 1000 , 2500 , 5000)),c(2,2,2),c(0),c(1),1)
+# funFIMpop("Emax*t/(t+D50)+E0",c("Emax","D50","E0"),c(30,500,5),c(0.09,0.09,0.09),c(1,0),list(c(0,100 , 300 , 500 , 1000 , 2500 , 5000)),c(2,2,2),c(0),c(1),1)
